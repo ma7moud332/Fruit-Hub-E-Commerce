952c1bfa53c799f13b93f3efafc3e740fa2a0952
ma7moud332 <119896210+ma7moud332@users.noreply.github.com>
2024-12-26 20:19:16 +0200

    splash view done

---
+++ lib/features/on_boarding/presentation/views/on_boarding_view.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class OnBoardingView extends StatelessWidget {
+  const OnBoardingView({super.key});
+
+  static const routeName = 'onBoarding';
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
